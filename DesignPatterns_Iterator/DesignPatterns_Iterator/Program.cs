81f3d56bd258db9eedefbe9c0bd632c594a6ce07
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-11-12 10:54:04 +0300

    Added iterator pattern implementation

--- DesignPatterns_Iterator/DesignPatterns_Iterator/Program.cs
+++ DesignPatterns_Iterator/DesignPatterns_Iterator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace DesignPatterns_Iterator
 {
@@ -7,6 +8,92 @@ namespace DesignPatterns_Iterator
         static void Main(string[] args)
         {
             Console.WriteLine("Design Patterns - Iterator!");
+            Library library = new Library();
+            Reader reader = new Reader();
+            reader.SeeBooks(library);
+            Console.Read();
+        }
+    }
+
+    class Reader
+    {
+        public void SeeBooks(Library library)
+        {
+            IIBookIterator iterator = library.CreateNumerator();
+            while (iterator.HasNext())
+            {
+                Book book = iterator.Next();
+                Console.WriteLine(book.Name);
+            }
+        }
+    }
+
+    class Book
+    {
+        public string Name { get; set; }
+    }
+
+    interface IIBookIterator
+    {
+        bool HasNext();
+        Book Next();
+    }
+
+    interface IBookNumerable
+    {
+        IIBookIterator CreateNumerator();
+        int Count { get; }
+        Book this[int index] { get; }
+    }
+
+    class Library : IBookNumerable
+    {
+        private Book[] _books;
+
+        public Library()
+        {
+            _books = new Book[]
+            {
+                new Book() {Name = "C# In Real Life"},
+                new Book() {Name = "Design Patters for Dummies"},
+                new Book() {Name = ".NET architecture with examples"}
+            };
+        }
+
+        public Book this[int index]
+        {
+            get { return _books[index]; }
+        }
+
+        public int Count
+        {
+            get { return _books.Length; }
+        }
+
+        public IIBookIterator CreateNumerator()
+        {
+            return new LibraryNumerator(this);
+        }
+    }
+
+    class LibraryNumerator : IIBookIterator
+    {
+        private IBookNumerable aggregate;
+        private int index = 0;
+
+        public LibraryNumerator(IBookNumerable a)
+        {
+            aggregate = a;
+        }
+
+        public bool HasNext()
+        {
+            return index < aggregate.Count;
+        }
+
+        public Book Next()
+        {
+            return aggregate[index++];
         }
     }
 }
