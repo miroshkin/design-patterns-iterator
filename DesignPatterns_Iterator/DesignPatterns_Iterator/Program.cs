e7b5a9603b7a2e1347cdc8f32a0dfc822bfc88eb
Anton Miroshkin <anton.miroshkin@gmail.com>
2019-11-12 10:20:56 +0300

    Added project

---
+++ DesignPatterns_Iterator/DesignPatterns_Iterator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DesignPatterns_Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Design Patterns - Iterator!");
+        }
+    }
+}
